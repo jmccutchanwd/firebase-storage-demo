919357324c044f778a7c4ead74aa5c55d5793749
John McCutchan <john.mccutchan.wd@gmail.com>
2017-01-31 11:54:40 -0600

    linked img to upload dynamically

--- app/app.js
+++ app/app.js
@@ -1,8 +1,18 @@
-
+/* John McCutchan ============================================ */
 console.log('Begin');
 const app = angular.module('MyApp',['ngRoute'])
-
+//
 app.config(($routeProvider, $locationProvider) => {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAhi1bEoFWrEeiCP_SvU89dcR3A3hltUPw",
+    authDomain: "practice-7fab7.firebaseapp.com",
+    databaseURL: "https://practice-7fab7.firebaseio.com",
+    storageBucket: "practice-7fab7.appspot.com",
+    messagingSenderId: "840921031785"
+  };
+  firebase.initializeApp(config);
+  //
   $locationProvider.hashPrefix('');
   $routeProvider
     .when('/', {
@@ -10,6 +20,7 @@ app.config(($routeProvider, $locationProvider) => {
       templateUrl: 'partials/home.html',
     })
 })
-
-
+//
 //'firebase'
+//firebase JSON: https://practice-7fab7.firebaseio.com/
+//firebase storage: gs://practice-7fab7.appspot.com
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,26 @@
 app.controller('HomeCtrl', function(){
   console.log('Home Ctrl');
+  // firebase root reference
+  let storageRef = firebase.storage().ref();
+  //
+  let inputElement = document.getElementById('fileInput');
+  //
+  inputElement.addEventListener("change", handleFiles, false);
+  //
+  function handleFiles() {
+    var fileList = this.files; /* now you can work with the file list */
+    console.log("filelist[0]", fileList[0])
+    storageRef.child(fileList[0].name).put(fileList[0])
+      .then(function(snapshot) {
+        console.log('Uploaded a blob or file!',snapshot, fileList[0].name);
+      });
+    storageRef.child(fileList[0].name).getDownloadURL()
+      .then(function(url) {
+        var img = document.getElementById('myImg');
+        img.src = url;
+        console.log(url);
+      }).catch(function(error) {
+        console.log(error)
+      });
+  }
 })
